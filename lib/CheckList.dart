8a9f715c605e5e905a2475bb473abbf43aa98821
d0rianb <dorian.beauchesne@icloud.com>
2021-05-05 19:16:26 +0200

    Add animation when hitting a tile

--- lib/CheckList.dart
+++ lib/CheckList.dart
@@ -8,6 +8,22 @@ import 'package:flutter/material.dart';
 
 typedef JSON = Map<String, dynamic>;
 
+class CircularClipper extends CustomClipper<Path> {
+  int percent;
+  CircularClipper(this.percent);
+
+  Path getClip(Size size) {
+    Path path = Path();
+    double ratio = percent / 100;
+    path.addOval(Rect.fromCenter(center: Offset(size.width / 2, size.height / 2), width: size.width * ratio, height: size.height * ratio));
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => true;
+}
+
+// ignore: must_be_immutable
 class CheckList extends StatelessWidget {
   String title = 'Unnamed list';
   List<CheckListItem> items = [];
@@ -47,47 +63,82 @@ class CheckListItem extends StatefulWidget {
   CheckListItemState createState() => CheckListItemState();
 }
 
-class CheckListItemState extends State<CheckListItem> {
+class CheckListItemState extends State<CheckListItem> with TickerProviderStateMixin {
   bool isChecked = false;
+  late AnimationController controller = AnimationController(vsync: this, duration: Duration(milliseconds: 175));
+  late Animation<int> animation = IntTween(begin: 0, end: 200).animate(controller);
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
-      margin: EdgeInsets.all(16),
-      clipBehavior: Clip.antiAlias,
-      child: InkWell(
-        splashColor: Colors.blue.withAlpha(30),
-        onTap: () {
-          setState(() => isChecked = !isChecked);
-        },
-        child: Column(
-          children: [
-            Expanded(
-              child: Container(
-                alignment: Alignment.bottomCenter,
-                foregroundDecoration: BoxDecoration(
-                  color: isChecked ? Colors.transparent : Colors.grey,
-                  backgroundBlendMode: BlendMode.saturation,
-                ),
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: widget.img.image,
-                    fit: BoxFit.cover,
+    return AnimatedBuilder(
+      builder: (context, anim) {
+        return Stack(children: <Widget>[
+          Card(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
+            margin: EdgeInsets.all(16),
+            clipBehavior: Clip.antiAlias,
+            child: Column(
+              children: [
+                Expanded(
+                  child: Stack(
+                    children: <Widget>[
+                      Container(
+                        alignment: Alignment.bottomCenter,
+                        foregroundDecoration: BoxDecoration(
+                          color: Colors.grey,
+                          backgroundBlendMode: BlendMode.saturation,
+                        ),
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: widget.img.image,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      ClipPath(
+                        clipBehavior: Clip.antiAlias,
+                        clipper: CircularClipper(isChecked ? 200 - animation.value : animation.value),
+                        child: Container(
+                          alignment: Alignment.bottomCenter,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: widget.img.image,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    widget.name,
+                    style: TextStyle(fontSize: 18),
+                  ),
+                )
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Text(
-                widget.name,
-                style: TextStyle(fontSize: 18),
-              ),
-            )
-          ],
-        ),
-      ),
+          ),
+          Material(
+            child: InkWell(
+                splashColor: Colors.grey.withAlpha(30),
+                onTap: () {
+                  setState(() => isChecked = !isChecked);
+                  controller.forward(from: 0);
+                }),
+            type: MaterialType.transparency,
+          ),
+        ]);
+      },
+      animation: controller,
     );
   }
 }
